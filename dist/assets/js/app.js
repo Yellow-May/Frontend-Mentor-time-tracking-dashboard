38df505841e7f66d07f5983969191d1a3e6e37e1
Yellow May <kwerep09@yahoo.com>
2021-10-05 19:09:56 +0100

    📦 NEW: Functionalities completed

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,4 +1,38 @@
 const fetchData = async () => await (await fetch("/dist/data.json")).json();
 const data = await fetchData();
 
-console.log(data);
+const currentArr = Array.from(document.getElementsByClassName("current"));
+const previousArr = Array.from(document.getElementsByClassName("previous"));
+
+const buttons = Array.from(
+	document.getElementsByClassName("buttons")[0].getElementsByTagName("button")
+);
+
+buttons.forEach(btn => {
+	btn.addEventListener("click", e => {
+		const { title } = e.target;
+
+		buttons.forEach(btnII =>
+			btnII === e.target ? btnII.classList.add("active") : btnII.classList.remove("active")
+		);
+
+		currentArr.forEach(curr => {
+			const { name } = curr.parentElement.parentElement.dataset;
+			const { current } = data.find(d => d.title === name).timeframes[title];
+
+			curr.textContent = `${current}hrs`;
+		});
+		previousArr.forEach(prev => {
+			const { name } = prev.parentElement.parentElement.dataset;
+			const { previous } = data.find(d => d.title === name).timeframes[title];
+			const content = () =>
+				title === "daily"
+					? `Yesterday - ${previous}hrs`
+					: title === "weekly"
+					? `Last Week - ${previous}hrs`
+					: `Last Month - ${previous}hrs`;
+
+			prev.textContent = content();
+		});
+	});
+});
